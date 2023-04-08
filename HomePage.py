e9237bef1e809d92abc888eb156592cfed3f96b3
Thilagan P <thilaganp1413@gmail.com>
2023-04-08 07:12:17 +0530

    Fixed kv

--- HomePage.py
+++ HomePage.py
@@ -2,9 +2,9 @@ import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
-from kivy.lang import Builder
+'''from kivy.lang import Builder
 
-Builder.load_file('homepage.kv')
+Builder.load_file('homepage.kv')'''
 
 class MyLayout(Widget):
     pass
