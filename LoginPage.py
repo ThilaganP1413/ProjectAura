51f408f1436a508c62924c66645a7c17da822098
Thilagan P <thilaganp1413@gmail.com>
2023-04-09 08:17:36 +0530

    Refined Homelayout

--- LoginPage.py
+++ LoginPage.py
@@ -3,8 +3,6 @@ from kivymd.app import MDApp
 
 class LoginPage(MDApp):
     def build(self):
-        #self.theme_cls.theme_style = "Light"
-        #self.theme_cls.primary_palette = "White"
         return Builder.load_file('loginpage.kv')
     
 LoginPage().run()
